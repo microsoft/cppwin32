058515b46cb874f58e46f1fd8e01c46228bdc6eb
Ryan Shepherd <ryansh@microsoft.com>
2020-10-21 19:24:51 -0700

    Emit #pragma region in the monomegaheader

--- cppwin32/main.cpp
+++ cppwin32/main.cpp
@@ -32,20 +32,38 @@ int main(int const argc, char* argv[])
         w.type_namespace = ns;
         {
             auto wrap = wrap_type_namespace(w, ns);
+
+            w.write("#pragma region enums\n");
             w.write_each<write_enum>(members.enums);
+            w.write("#pragma endregion enums\n\n");
+
+            w.write("#pragma region forward_declarations\n");
             w.write_each<write_forward>(members.structs);
             w.write_each<write_forward>(members.interfaces);
-            w.write_each<write_delegate>(members.delegates);
+            w.write("#pragma endregion forward_declarations\n\n");
 
+            w.write("#pragma region delegates\n");
+            w.write_each<write_delegate>(members.delegates);
+            w.write("#pragma endregion delegates\n\n");
+
+            w.write("#pragma region structs\n");
             write_structs(w, members.structs);
+            w.write("#pragma endregion structs\n\n");
         }
         {
+            w.write("#pragma region abi_methods\n");
             w.write_each<write_class_abi>(members.classes);
+            w.write("#pragma endregion abi_methods\n\n");
         }
         {
             auto wrap = wrap_type_namespace(w, ns);
+            w.write("#pragma region methods\n");
             w.write_each<write_class>(members.classes);
+            w.write("#pragma endregion methods\n\n");
+
+            w.write("#pragma region enum_operators\n");
             w.write_each<write_enum_operators>(members.enums);
+            w.write("#pragma endregion enum_operators\n\n");
         }
 
         w.save_header(o.output_folder.string());
